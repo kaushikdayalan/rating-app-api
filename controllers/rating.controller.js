468444a7f01a9d2257086ccb65d8fec1efc15967
Kaushik <kaushik@Kaushiks-MacBook-Air.local>
2021-08-17 18:47:22 +0530

    added fetch all details

--- controllers/rating.controller.js
+++ controllers/rating.controller.js
@@ -6,39 +6,49 @@ const addName = async (req, res) => {
         name: req.body.childName
     });
     Rating.save()
-    .then(()=>{
-        res.json({status: 200, message: "Child added sucessfuly"});
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.json({status: 400, error: "Child not added please try again"});
-    });
+        .then(() => {
+            res.json({ status: 200, message: "Child added sucessfuly" });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({ status: 400, error: "Child not added please try again" });
+        });
 }
 
-const IncreaseRating = async (req, res) =>{
+const getChildrenDetails = async (req, res) => {
+    RatingModel.find()
+        .then(result => {
+            res.json({ status: 200, data: result });
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+const IncreaseRating = async (req, res) => {
     const updatedRating = await RatingModel.findById(req.params.id);
     updatedRating.rating = updatedRating.rating + 1;
     updatedRating.save()
-    .then(()=>{
-        res.json({statu: 200, message: "Rating Increased"});
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.json({status: 400, error: "Cannot increase rating try again"});
-    })
+        .then(() => {
+            res.json({ statu: 200, message: "Rating Increased" });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({ status: 400, error: "Cannot increase rating try again" });
+        })
 };
 
-const DecreaseRating = async (req, res) =>{
+const DecreaseRating = async (req, res) => {
     const updatedRating = await RatingModel.findById(req.params.id);
     updatedRating.rating = updatedRating.rating - 1;
     updatedRating.save()
-    .then(()=>{
-        res.json({statu: 200, message: "Rating Increased"});
-    })
-    .catch((err)=>{
-        console.log(err);
-        res.json({status: 400, error: "Cannot increase rating try again"});
-    })
+        .then(() => {
+            res.json({ statu: 200, message: "Rating Increased" });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.json({ status: 400, error: "Cannot increase rating try again" });
+        })
 }
 
-module.exports = {addName, IncreaseRating, DecreaseRating};
+module.exports = { addName, IncreaseRating, DecreaseRating, getChildrenDetails };
--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const RatingController = require('../controllers/rating.controller');
 
 router.post('/addName', [RatingController.addName]);
+router.get('/getDetails', [RatingController.getChildrenDetails]);
 router.patch('/increment/:id',[RatingController.IncreaseRating]);
 router.patch('/decrement/:id', [RatingController.DecreaseRating]);
 
